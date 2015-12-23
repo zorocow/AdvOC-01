daa30886c56874b4464386336cf0d1b18f73a04b
zorocow <zorocow@gmail.com>
2015-12-23 18:22:24 +0200

    day 1 is done

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use std::fs;
 
 fn main() {
     let mut s: String = String::new();
-    let mut f = fs::File::open("input")
-        .ok()
-        .expect("Can't find file \"input\"");
+    let mut f = match fs::File::open("input") {
+        Err(e) => panic!("Can't open file \"{}\": {}", "input", e),
+        Ok(f) => f,
+    };
     f.read_to_string(&mut s).ok().expect("can't read string");
-    println!("{}", s);
+    let floor = s.as_bytes().iter().fold(0i64, |acc: i64, &c| match c {
+        b'(' => acc+1,
+        b')' => acc-1,
+        _ => acc,
+    });
+    println!("Santa should go to floor {}", floor);
 }
