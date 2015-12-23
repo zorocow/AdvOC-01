6df4b38b7e5c9e34de417b308f4426f5a5557306
zorocow <zorocow@gmail.com>
2015-12-23 20:18:11 +0200

    day 2 part 2

--- src/bin/adv2.rs
+++ src/bin/adv2.rs
@@ -14,8 +14,20 @@ fn calc_areas(s: &[u32; 3]) -> [u32; 3] {
     ]
 }
 
-fn calc_slack(s: &[u32; 3]) -> u32 {
-    calc_areas(s).iter().fold(u32::max_value(), |acc, &n| match n < acc {
+fn calc_circum(s: &[u32; 3]) -> [u32; 3] {
+    [
+        2*(s[0] + s[1]),
+        2*(s[1] + s[2]),
+        2*(s[2] + s[0]),
+    ]
+}
+
+fn calc_volume(s: &[u32; 3]) -> u32 {
+    s[0] * s[1] * s[2]
+}
+
+fn min(s: &[u32; 3]) -> u32 {
+    s.iter().fold(u32::max_value(), |acc, &n| match n < acc {
         true => n,
         false => acc,
     })
@@ -25,6 +37,8 @@ fn calc_total_area(s: &[u32; 3]) -> u32 {
     calc_areas(s).iter().fold(0, |acc, &area| 2 * area + acc)
 }
 
+
+
 fn main() {
     let f = match File::open(FILENAME) {
         Err(e) => panic!("Can't open file \"{}\": {}", FILENAME, e),
@@ -33,6 +47,7 @@ fn main() {
     let reader = BufReader::new(f);
 
     let mut area = 0;
+    let mut ribbon = 0;
     for line in reader.lines() {
         let line = line.unwrap();
         if line.len() == 0 {
@@ -44,7 +59,10 @@ fn main() {
         let sizes: [u32; 3] = [sizes[0].parse().unwrap(),
                                sizes[1].parse().unwrap(),
                                sizes[2].parse().unwrap(),];
-        area += calc_slack(&sizes) + calc_total_area(&sizes);
+        area += min(&calc_areas(&sizes)) + calc_total_area(&sizes);
+        ribbon += min(&calc_circum(&sizes)) + calc_volume(&sizes);
     }
     println!("The elves need {} square feet of wrapping paper", area);
+    println!("The elves need {} feet of ribbon", ribbon);
+
 }
