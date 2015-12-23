c94b39bf075edcae6a46fb4427e22961098b1636
zorocow <zorocow@gmail.com>
2015-12-23 19:42:46 +0200

    day 1 part 2 complete

--- src/bin/adv1.rs
+++ src/bin/adv1.rs
@@ -10,10 +10,26 @@ fn main() {
         Ok(f) => f,
     };
     f.read_to_string(&mut s).ok().expect("can't read string");
-    let floor = s.chars().fold(0i64, |acc: i64, c| match c {
-        '(' => acc+1,
-        ')' => acc-1,
-        _ => acc,
-    });
+//    let floor = s.chars().fold(0i64, |acc: i64, c| match c {
+//        '(' => acc+1,
+//        ')' => acc-1,
+//        _ => acc,
+//    });
+    let mut first_minus_1_location = 0i64;
+    let mut floor = 0i64;
+    let mut i = 0i64;
+    for c in s.chars() {
+        i+=1;
+        floor += match c {
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
+        };
+        if first_minus_1_location == 0 && floor == -1 {
+            first_minus_1_location = i;
+        }
+    }
+
     println!("Santa should go to floor {}", floor);
+    println!("Fanta first entered floor -1 at step {}", first_minus_1_location);
 }
