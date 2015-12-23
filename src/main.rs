7293787ecb2765996b9042110f9f1117260f7e75
zorocow <zorocow@gmail.com>
2015-12-23 17:56:07 +0200

    implement read `input` to string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
+use std::io::prelude::*;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut s: String = String::new();
+    let mut f = fs::File::open("input")
+        .ok()
+        .expect("Can't find file \"input\"");
+    f.read_to_string(&mut s).ok().expect("can't read string");
+    println!("{}", s);
 }
