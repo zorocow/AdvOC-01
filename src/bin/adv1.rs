811d28bea53960d938ea75b4300e7f38e1bf07fe
zorocow <zorocow@gmail.com>
2015-12-23 18:47:20 +0200

    fixed day 1 input; switched byte iterator to char

--- src/bin/adv1.rs
+++ src/bin/adv1.rs
@@ -2,16 +2,17 @@
 use std::io::prelude::*;
 use std::fs;
 
+const FILENAME: &'static str = "input_d1";
 fn main() {
     let mut s: String = String::new();
-    let mut f = match fs::File::open("input") {
-        Err(e) => panic!("Can't open file \"{}\": {}", "input", e),
+    let mut f = match fs::File::open(FILENAME) {
+        Err(e) => panic!("Can't open file \"{}\": {}", FILENAME, e),
         Ok(f) => f,
     };
     f.read_to_string(&mut s).ok().expect("can't read string");
-    let floor = s.as_bytes().iter().fold(0i64, |acc: i64, &c| match c {
-        b'(' => acc+1,
-        b')' => acc-1,
+    let floor = s.chars().fold(0i64, |acc: i64, c| match c {
+        '(' => acc+1,
+        ')' => acc-1,
         _ => acc,
     });
     println!("Santa should go to floor {}", floor);
